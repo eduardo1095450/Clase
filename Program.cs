d42903c0611b8094f4dc8919e7b19a58684a6427
eduardo1095450 <eduardodelrosariog@gmail.com>
2021-10-29 19:30:41 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace Clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas = 0;
+            int minutos = 0;
+            int segundos = 0;
+            Console.WriteLine("Segundos");
+            segundos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Minutos");
+            minutos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Horas");
+            horas = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i > -1; i++)
+            {
+                if (segundos>=0)
+                {
+                    segundos--;
+                    Thread.Sleep(1000);
+                }
+                if (minutos > 0 && segundos == 0)
+                {
+                    minutos--;
+                    segundos = 59;
+                }
+                if (horas > 0 && minutos == 0 && segundos == 0)
+                {
+                    horas--;
+                    minutos = 59;
+                    segundos = 59;
+                }
+                if (horas == 0 && minutos == 0 && segundos == 0)
+                {
+                    Console.WriteLine("Ya termino el tiempo");
+
+                    i = -3;
+                }
+                Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+            }
+        }
+    }
+}
